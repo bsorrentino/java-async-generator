4b638afcefd37f829c9eacbca4b1d423582bc53d
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-12-02 14:38:39 +0100

    chore: clean code

--- src/main/java/org/bsc/async/AsyncGenerator.java
+++ src/main/java/org/bsc/async/AsyncGenerator.java
@@ -192,91 +192,6 @@ public interface AsyncGenerator<E> extends Iterable<E> {
     Data<E> next();
 
     /**
-     * Returns an empty AsyncGenerator.
-     *
-     * @param <E> the type of elements
-     * @return an empty AsyncGenerator
-     */
-    static <E> AsyncGenerator<E> empty() {
-        return Data::done;
-    }
-
-    /**
-     * create a generator, mapping each element  to an asynchronous counterpart.
-     *
-     * @param <E> the type of elements in the collection
-     * @param <U> the type of elements in the CompletableFuture
-     * @param iterator the elements iterator
-     * @param mapFunction the function to map elements to {@link  java.util.concurrent.CompletableFuture}
-     * @return an AsyncGenerator instance with mapped elements
-     */
-    static <E,U> AsyncGenerator<U> map(Iterator<E> iterator, Function<E, CompletableFuture<U>> mapFunction ) {
-        return () -> {
-            if( !iterator.hasNext() ) {
-                return Data.done();
-            }
-            return Data.of(mapFunction.apply( iterator.next() ));
-        };
-    }
-
-    /**
-     * Collects asynchronous elements from an iterator.
-     *
-     * @param <E> the type of elements in the iterator
-     * @param <U> the type of elements in the CompletableFuture
-     * @param iterator the iterator containing elements to collect
-     * @param consumer the function to consume elements and add them to the accumulator
-     * @return an AsyncGenerator instance with collected elements
-     */
-    static <E,U> AsyncGenerator<U> collect(Iterator<E> iterator, BiConsumer<E, Consumer<CompletableFuture<U>>> consumer ) {
-        final List<CompletableFuture<U>> accumulator = new ArrayList<>();
-
-        final Consumer<CompletableFuture<U>> addElement = accumulator::add;
-        while( iterator.hasNext() ) {
-            consumer.accept(iterator.next(), addElement );
-        }
-
-        final Iterator<CompletableFuture<U>> it = accumulator.iterator();
-        return () -> {
-            if( !it.hasNext() ) {
-                return Data.done();
-            }
-            return Data.of(it.next());
-        };
-    }
-
-    /**
-     * create a generator, mapping each element  to an asynchronous counterpart.
-     *
-     * @param <E> the type of elements in the collection
-     * @param <U> the type of elements in the CompletableFuture
-     * @param collection the collection of elements to map
-     * @param mapFunction the function to map elements to CompletableFuture
-     * @return an AsyncGenerator instance with mapped elements
-     */
-    static <E,U> AsyncGenerator<U> map( Collection<E> collection, Function<E,CompletableFuture<U>> mapFunction ) {
-        if( collection == null || collection.isEmpty()) {
-            return empty();
-        }
-        return map( collection.iterator(), mapFunction);
-    }
-    /**
-     * Collects asynchronous elements from a collection.
-     *
-     * @param <E> the type of elements in the iterator
-     * @param <U> the type of elements in the CompletableFuture
-     * @param collection the iterator containing elements to collect
-     * @param consumer the function to consume elements and add them to the accumulator
-     * @return an AsyncGenerator instance with collected elements
-     */
-    static <E,U> AsyncGenerator<U> collect( Collection<E> collection, BiConsumer<E, Consumer<CompletableFuture<U>>> consumer ) {
-        if( collection == null || collection.isEmpty()) {
-            return empty();
-        }
-        return collect( collection.iterator(), consumer);
-    }
-
-    /**
      * Converts the AsyncGenerator to a CompletableFuture.
      *
      * @return a CompletableFuture representing the completion of the AsyncGenerator
@@ -378,6 +293,92 @@ public interface AsyncGenerator<E> extends Iterable<E> {
      */
     default Iterator<E> iterator() {
         return new InternalIterator<E>( this );
+    }
+
+
+    /**
+     * Returns an empty AsyncGenerator.
+     *
+     * @param <E> the type of elements
+     * @return an empty AsyncGenerator
+     */
+    static <E> AsyncGenerator<E> empty() {
+        return Data::done;
+    }
+
+    /**
+     * create a generator, mapping each element  to an asynchronous counterpart.
+     *
+     * @param <E> the type of elements in the collection
+     * @param <U> the type of elements in the CompletableFuture
+     * @param iterator the elements iterator
+     * @param mapFunction the function to map elements to {@link  java.util.concurrent.CompletableFuture}
+     * @return an AsyncGenerator instance with mapped elements
+     */
+    static <E,U> AsyncGenerator<U> map(Iterator<E> iterator, Function<E, CompletableFuture<U>> mapFunction ) {
+        return () -> {
+            if( !iterator.hasNext() ) {
+                return Data.done();
+            }
+            return Data.of(mapFunction.apply( iterator.next() ));
+        };
+    }
+
+    /**
+     * Collects asynchronous elements from an iterator.
+     *
+     * @param <E> the type of elements in the iterator
+     * @param <U> the type of elements in the CompletableFuture
+     * @param iterator the iterator containing elements to collect
+     * @param consumer the function to consume elements and add them to the accumulator
+     * @return an AsyncGenerator instance with collected elements
+     */
+    static <E,U> AsyncGenerator<U> collect(Iterator<E> iterator, BiConsumer<E, Consumer<CompletableFuture<U>>> consumer ) {
+        final List<CompletableFuture<U>> accumulator = new ArrayList<>();
+
+        final Consumer<CompletableFuture<U>> addElement = accumulator::add;
+        while( iterator.hasNext() ) {
+            consumer.accept(iterator.next(), addElement );
+        }
+
+        final Iterator<CompletableFuture<U>> it = accumulator.iterator();
+        return () -> {
+            if( !it.hasNext() ) {
+                return Data.done();
+            }
+            return Data.of(it.next());
+        };
+    }
+
+    /**
+     * create a generator, mapping each element  to an asynchronous counterpart.
+     *
+     * @param <E> the type of elements in the collection
+     * @param <U> the type of elements in the CompletableFuture
+     * @param collection the collection of elements to map
+     * @param mapFunction the function to map elements to CompletableFuture
+     * @return an AsyncGenerator instance with mapped elements
+     */
+    static <E,U> AsyncGenerator<U> map( Collection<E> collection, Function<E,CompletableFuture<U>> mapFunction ) {
+        if( collection == null || collection.isEmpty()) {
+            return empty();
+        }
+        return map( collection.iterator(), mapFunction);
+    }
+    /**
+     * Collects asynchronous elements from a collection.
+     *
+     * @param <E> the type of elements in the iterator
+     * @param <U> the type of elements in the CompletableFuture
+     * @param collection the iterator containing elements to collect
+     * @param consumer the function to consume elements and add them to the accumulator
+     * @return an AsyncGenerator instance with collected elements
+     */
+    static <E,U> AsyncGenerator<U> collect( Collection<E> collection, BiConsumer<E, Consumer<CompletableFuture<U>>> consumer ) {
+        if( collection == null || collection.isEmpty()) {
+            return empty();
+        }
+        return collect( collection.iterator(), consumer);
     }
 
 }
