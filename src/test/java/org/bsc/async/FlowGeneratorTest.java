09adc3cd805d03d2923eb54f2bf0cf83a0b6faca
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-01-21 19:50:58 +0100

    test: add test for reactive stream integration

---
+++ src/test/java/org/bsc/async/FlowGeneratorTest.java
@@ -0,0 +1,102 @@
+package org.bsc.async;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static java.util.concurrent.CompletableFuture.runAsync;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FlowGeneratorTest {
+
+    @Test
+    public void flowGeneratorSubscriberTest() throws Exception {
+
+        var executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(20);
+
+        var publisher = new SubmissionPublisher<String>();
+
+        final var data = List.of( "e1", "e2", "e3", "e4", "e5" );
+
+        var generator = FlowGenerator.fromPublisher(publisher);
+
+        assertTrue( publisher.hasSubscribers() );
+
+        var submitting = runAsync( () -> {
+                data.stream().peek(System.out::println).forEach( publisher::submit );
+                publisher.close();
+            }, executor );
+
+        final List<String> result = new ArrayList<>();
+        var iterating = generator.async( executor ).forEachAsync(result::add);
+
+        CompletableFuture.allOf(iterating, submitting ).join();
+
+        assertEquals( data.size(), result.size() );
+        assertIterableEquals( data, result );
+
+        System.out.println("Core pool size: " + executor.getCorePoolSize());
+        System.out.println("Largest pool size: " + executor.getLargestPoolSize());
+        System.out.println("Active threads: " + executor.getActiveCount());
+        System.out.println("Completed tasks: " + executor.getCompletedTaskCount());
+
+    }
+
+    @Test
+    public void flowGeneratorPublisherTest() throws Exception {
+
+        var executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(20);
+
+        var queue = new LinkedBlockingQueue<AsyncGenerator.Data<String>>();
+
+        final var data = List.of( "e1", "e2", "e3", "e4", "e5" );
+
+        var generator = AsyncGeneratorQueue.of( queue, q -> {
+
+            for( String value: data ) {
+                queue.add(AsyncGenerator.Data.of(completedFuture(value)));
+            }
+        }, executor);
+
+        var publisher = FlowGenerator.toPublisher( generator );
+
+        var result = new ArrayList<String>();
+
+        publisher.subscribe(new Flow.Subscriber<String>() {
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {
+                subscription.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(String item) {
+                result.add(item);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.getLocalizedMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Completed");
+            }
+        });
+
+        generator.toCompletableFuture().join();
+
+        assertEquals( data.size(), result.size() );
+        assertIterableEquals( data, result );
+
+        System.out.println("Core pool size: " + executor.getCorePoolSize());
+        System.out.println("Largest pool size: " + executor.getLargestPoolSize());
+        System.out.println("Active threads: " + executor.getActiveCount());
+        System.out.println("Completed tasks: " + executor.getCompletedTaskCount());
+
+    }
+
+}
