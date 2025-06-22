49ae7dd5cbed212aec341592783e84803013abc7
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-06-22 13:22:00 +0200

    refactor: replace ReentrantReadWriteLock with AtomicReference
    - Introduced `AtomicReference` to manage the current fetched data, simplifying the locking mechanism and improving performance.
    - Removed unnecessary read and write locks, reducing lock contention and enhancing concurrency.
    
    resolve #3

--- src/main/java/org/bsc/async/AsyncGenerator.java
+++ src/main/java/org/bsc/async/AsyncGenerator.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -362,47 +362,31 @@ public interface AsyncGenerator<E> extends Iterable<E>, AsyncGeneratorOperators<
 
 class InternalIterator<E> implements Iterator<E> {
     private final AsyncGenerator<E> delegate;
-    private AsyncGenerator.Data<E> currentFetchedData;
-    private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
-    private final ReentrantReadWriteLock.ReadLock r = rwl.readLock();
-    private final ReentrantReadWriteLock.WriteLock w = rwl.writeLock();
 
+    final AtomicReference<AsyncGenerator.Data<E>> currentFetchedData;
 
     public InternalIterator(AsyncGenerator<E> delegate) {
         this.delegate = delegate;
-        currentFetchedData = delegate.next();
+        currentFetchedData = new AtomicReference<>(delegate.next());
     }
     @Override
     public boolean hasNext() {
-        try {
-            r.lock();
-            final AsyncGenerator.Data<E> value = currentFetchedData;
-            return value != null && !value.isDone();
-        }
-        finally {
-            r.unlock();
-        }
+        final var value = currentFetchedData.get();
+        return value != null && !value.isDone();
     }
 
     @Override
     public E next() {
-        try {
-            w.lock();
+        var next = currentFetchedData.get();
 
-            AsyncGenerator.Data<E> next = currentFetchedData;
-
-            if( next==null || next.isDone() ) {
-                throw new IllegalStateException("no more elements into iterator");
-            }
-
-            if( !next.isError() ) {
-                currentFetchedData = delegate.next();
-            }
-
-            return next.data.join();
+        if( next==null || next.isDone() ) {
+            throw new IllegalStateException("no more elements into iterator");
         }
-        finally {
-            w.unlock();
+
+        if( !next.isError() ) {
+            currentFetchedData.set( delegate.next() );
         }
+
+        return next.data.join();
     }
 };
